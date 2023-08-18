0786578407e6396a11030631a9c57ff1c67cb6ac
Dani Perez <daniperez@users.sourceforge.net>
2023-08-18 21:14:03 +0200

    Implementing temporary file management as explained in [1] (#10)
    Plus minor linting fixes.
    
    [1] https://github.com/daniperez/vale-python-package/pull/7#discussion_r1181080468

--- vale/main.py
+++ vale/main.py
@@ -11,7 +11,7 @@ import tempfile
 import zipfile
 from functools import partial
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Tuple
 from urllib.request import urlopen
 
 import vale
@@ -23,7 +23,7 @@ else:
 
 
 def major_minor_patch(version: str) -> str:
-    """E.g.: 2.20.0.1 -> 2.20.0"""
+    """E.g.: 2.20.0.1 -> 2.20.0 ."""
     return ".".join(version.split(".")[0:3])
 
 
@@ -35,7 +35,7 @@ def major_minor_patch(version: str) -> str:
 vale_bin_version = major_minor_patch(importlib_metadata.version("vale"))
 
 
-def get_target() -> (str, str, str):
+def get_target() -> Tuple[str, str, str]:
     """Return Vale's target OS, architecture and extension to download."""
     operating_system: Optional[str] = None
     architecture: Optional[str] = None
@@ -73,16 +73,16 @@ def get_target() -> (str, str, str):
 
 
 def extract_vale(
-    archive: str, archive_type: str, destination: str, bin_name: str = "vale"
+    archive: str, extension: str, destination: str, bin_name: str = "vale"
 ) -> str:
     """Extract `vale` binary from the given archive."""
-    if archive_type == "zip":
+    if extension == "zip":
         archiver = zipfile.ZipFile
-    elif archive_type == "tar.gz":
-        archiver = partial(tarfile.open, mode="r:gz")
+    elif extension == "tar.gz":
+        archiver = partial(tarfile.open, mode="r:gz")  # type: ignore
     else:
         raise ValueError(
-            f"Archive type '{archive_type}' is not supported. "
+            f"Archive type '{extension}' is not supported. "
             "Only 'zip' and 'tar.gz' supported."
         )
 
@@ -100,9 +100,9 @@ def download_vale_if_missing() -> str:
     """Download vale only if missing."""
     vale_bin_path = Path(vale.__file__).parent / "vale_bin"
 
-    # We have a dummy vale placeholder that is overwritten by the downloaded vale version.
-    # See `vale/vale_bin` (in this repo, not in its installed form) for more details about
-    # this magic number of bytes.
+    # We have a dummy vale placeholder that is overwritten by the downloaded
+    # vale version. See `vale/vale_bin` (in this repo, not in its installed
+    # form) for more details about this magic number of bytes.
     if vale_bin_path.stat().st_size < 1000:
 
         print("* vale not found. Downloading it...")
@@ -118,28 +118,35 @@ def download_vale_if_missing() -> str:
 
         url = urlopen(url_str)
 
-        # delete=False is required to avoid permissions errors on windows
-        with tempfile.NamedTemporaryFile(mode="w+b", delete=False) as tp:
+        with tempfile.TemporaryDirectory() as temp_dir:
 
-            tp.write(url.read())
+            temp_dir_path = Path(temp_dir)
 
-            print(f"* {url_str} downloaded to {tp.name}")
+            archive_temp_file_path = temp_dir_path / "vale.zip"
 
-            with tempfile.TemporaryDirectory() as td:
+            with open(str(archive_temp_file_path), "wb") as archive_temp_file:
+
+                archive_temp_file.write(url.read())
+
+                print(f"* {url_str} downloaded to {archive_temp_file.name}")
+
+                archive_temp_dir_path = temp_dir_path / "vale_unzipped"
+
+                archive_temp_dir_path.mkdir()
 
                 archive_bin_name = (
                     "vale.exe" if operating_system == "Windows" else "vale"
                 )
-                vale_tmp_path = extract_vale(tp.name, extension, td, archive_bin_name)
+
+                vale_tmp_path = extract_vale(
+                    archive=str(archive_temp_file_path),
+                    extension=extension,
+                    destination=str(archive_temp_dir_path),
+                    bin_name=archive_bin_name,
+                )
 
                 print(f"* Copying {vale_tmp_path} to {vale_bin_path}")
                 shutil.copy(f"{vale_tmp_path}", f"{vale_bin_path}")
-
-        # clean up the temp file if it still exists
-        try:
-            os.unlink(tp.name)
-        except Exception:
-            pass
 
         print("* vale extracted and copied to module path.")
 
